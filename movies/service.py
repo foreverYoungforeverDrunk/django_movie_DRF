b9dbcb2c08f665361070953e34830bfd53d0fd61
Денис Киндрук <thunder03026@gmail.com>
2021-07-26 21:29:20 +0300

    add docs, add social login, add ViewSets

--- movies/service.py
+++ movies/service.py
@@ -3,7 +3,7 @@ from .models import Movie
 
 
 def get_client_ip(request):
-    """Получение IP пользоваеля"""
+    """Получение IP пользователя"""
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0]
